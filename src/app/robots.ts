2927d0f65bc2121ac74b84524f67f68c637cc2e9
Godjeksatouu <godjeksatou@gmail.com>
2026-04-08 07:28:58 +0100

    fix: remove invalid directive from robots.txt (Content-Signal)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,18 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/private/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/private/'],
+      },
+      {
+        userAgent: 'GPTBot',
+        disallow: '/',
+      },
+    ],
     sitemap: 'https://gaoshouke.com/sitemap.xml',
   };
 }
+
